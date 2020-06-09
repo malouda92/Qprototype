9d262d417f919992766c736823429a97ba7bf7c0
Malouda92 <ramalomi@gmail.com>
2020-06-09 16:20:26 +0300

    commit

--- app/src/main/java/com/cnaps/ramanandafylovamialy/qprototype/Main3Activity.java
+++ app/src/main/java/com/cnaps/ramanandafylovamialy/qprototype/Main3Activity.java
@@ -3,6 +3,9 @@ package com.cnaps.ramanandafylovamialy.qprototype;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -15,6 +18,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.cnaps.ramanandafylovamialy.lib.Enquete;
 import com.cnaps.ramanandafylovamialy.lib.OnTaskCompleted;
@@ -42,11 +46,16 @@ public class Main3Activity extends AppCompatActivity implements AdapterView.OnIt
     EditText text_nom = null;
     EditText text_pourquoi = null;
     RadioGroup group = null;
+    ProgressDialog pb= null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        pb = new ProgressDialog(this);
+        pb.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        pb.setMessage("en cours");
 
         listeTypeAssujettis = new ArrayList<>();
         listeTypePrestations = new ArrayList<>();
@@ -68,18 +77,20 @@ public class Main3Activity extends AppCompatActivity implements AdapterView.OnIt
         serviceHTTPForAssuj.setListener(new OnTaskCompleted() {
             @Override
             public void onTaskCompleted(String response) {
-                try{
-                    JSONArray jsa = new JSONArray(response);
-                    for (int i = 0; i < jsa.length(); i++) {
-                        JSONObject current = jsa.getJSONObject(i);
-                        if (current.getInt("id_type") > 0 && !current.getString("lib_type").isEmpty()) {
-                            listeTypeAssujettis.add(new TypeAssujettis(current.getInt("id_type"), current.getString("lib_type")));
-                            ArrayAdapter<TypeAssujettis> adapterForAssuj = new ArrayAdapter<>(Main3Activity.this, android.R.layout.simple_spinner_dropdown_item, listeTypeAssujettis);
-                            select_assuj.setAdapter(adapterForAssuj);
+                if(response != null) {
+                    try {
+                        JSONArray jsa = new JSONArray(response);
+                        for (int i = 0; i < jsa.length(); i++) {
+                            JSONObject current = jsa.getJSONObject(i);
+                            if (current.getInt("id_type") > 0 && !current.getString("lib_type").isEmpty()) {
+                                listeTypeAssujettis.add(new TypeAssujettis(current.getInt("id_type"), current.getString("lib_type")));
+                                ArrayAdapter<TypeAssujettis> adapterForAssuj = new ArrayAdapter<>(Main3Activity.this, android.R.layout.simple_spinner_dropdown_item, listeTypeAssujettis);
+                                select_assuj.setAdapter(adapterForAssuj);
+                            }
                         }
+                    } catch (JSONException e) {
+                        Log.e("erreur", e.getMessage());
                     }
-                }catch (JSONException e){
-                    Log.e("erreur", e.getMessage());
                 }
             }
         });
@@ -89,18 +100,20 @@ public class Main3Activity extends AppCompatActivity implements AdapterView.OnIt
         serviceHTTPForPresta.setListener(new OnTaskCompleted() {
             @Override
             public void onTaskCompleted(String response) {
-                try{
-                    JSONArray jsa = new JSONArray(response);
-                    for (int i = 0; i < jsa.length(); i++) {
-                        JSONObject current = jsa.getJSONObject(i);
-                        if (!current.getString("prestation_code").isEmpty() && !current.getString("prestation_libelle").isEmpty()) {
-                            listeTypePrestations.add(new TypePrestation(current.getString("prestation_code"), current.getString("prestation_libelle")));
-                            ArrayAdapter<TypePrestation> adapterForPresta = new ArrayAdapter<>(Main3Activity.this, android.R.layout.simple_spinner_dropdown_item, listeTypePrestations);
-                            select_presta.setAdapter(adapterForPresta);
+                if(response != null) {
+                    try {
+                        JSONArray jsa = new JSONArray(response);
+                        for (int i = 0; i < jsa.length(); i++) {
+                            JSONObject current = jsa.getJSONObject(i);
+                            if (!current.getString("prestation_code").isEmpty() && !current.getString("prestation_libelle").isEmpty()) {
+                                listeTypePrestations.add(new TypePrestation(current.getString("prestation_code"), current.getString("prestation_libelle")));
+                                ArrayAdapter<TypePrestation> adapterForPresta = new ArrayAdapter<>(Main3Activity.this, android.R.layout.simple_spinner_dropdown_item, listeTypePrestations);
+                                select_presta.setAdapter(adapterForPresta);
+                            }
                         }
+                    } catch (JSONException e) {
+                        Log.e("erreur", e.getMessage());
                     }
-                }catch (JSONException e){
-                    Log.e("erreur", e.getMessage());
                 }
             }
         });
@@ -122,18 +135,27 @@ public class Main3Activity extends AppCompatActivity implements AdapterView.OnIt
                         enquete.setRps(text_pourquoi.getText().toString());
                         JSONObject jso = new JSONObject();
                         try {
-                            jso.put("type_assujettis", enquete.getType_assujetti());
+                            jso.put("id_type", enquete.getType_assujetti());
                             jso.put("matricule", enquete.getMatricule());
-                            jso.put("type_presta", enquete.getType_prestation());
+                            jso.put("prestation_code", enquete.getType_prestation());
                             jso.put("choix", enquete.getId_radio());
                             jso.put("pourquoi", enquete.getRps());
 
+                            pb.show();
                             ServiceHTTPPost serviceHTTPForPOST = new ServiceHTTPPost();
-                            serviceHTTPForPOST.execute("", jso.toString());
+                            serviceHTTPForPOST.execute("http://192.168.6.247:3300/reponses/rps/ajout", jso.toString());
                             serviceHTTPForPOST.setListener(new OnTaskCompleted() {
                                 @Override
                                 public void onTaskCompleted(String response) {
-
+                                    pb.hide();
+                                    new AlertDialog.Builder(Main3Activity.this).setTitle("success").setMessage("Merci de votre collaboration").setCancelable(false).setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent = getIntent();
+                                            finish();
+                                            startActivity(intent);
+                                        }
+                                    }).show();
                                 }
                             });
                         } catch (JSONException e) {
@@ -156,22 +178,62 @@ public class Main3Activity extends AppCompatActivity implements AdapterView.OnIt
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 String assujettis = select_assuj.getSelectedItem().toString();
                 if (assujettis.equals("TRAVAILLEUR")) {
-
+                    ServiceHTTPGet serviceHTTPForMat = new ServiceHTTPGet();
+                    serviceHTTPForMat.execute("http://192.168.6.247:3300/traval/trav/selectlike/" + s);
+                    serviceHTTPForMat.setListener(new OnTaskCompleted() {
+                        @Override
+                        public void onTaskCompleted(String response) {
+                            if(response != null) {
+                                try {
+                                    JSONObject jso = new JSONObject(response);
+                                    if (!jso.getString("travailleur_nom").isEmpty()) {
+                                        text_nom.setText(jso.getString("travailleur_nom"));
+                                    }else{
+                                        text_nom.setText("");
+                                    }
+                                }catch (JSONException e) {
+                                    Log.e("erreur", e.getMessage());
+                                }
+                            }
+                        }
+                    });
                 }else if(assujettis.equals("EMPLOYEUR")) {
                     ServiceHTTPGet serviceHTTPForMat = new ServiceHTTPGet();
                     serviceHTTPForMat.execute("http://192.168.6.247:3300/emplo/empl/select/" + s);
                     serviceHTTPForMat.setListener(new OnTaskCompleted() {
                         @Override
                         public void onTaskCompleted(String response) {
-                            try{
-                                JSONObject jso = new JSONObject(response);
-                                if (!jso.getString("temployeur_nom").isEmpty()) {
-                                    text_nom.setText(jso.getString("temployeur_nom"));
-                                }else{
-                                    text_nom.setText("");
+                            if(response != null) {
+                                try {
+                                    JSONObject jso = new JSONObject(response);
+                                    if (!jso.getString("temployeur_nom").isEmpty()) {
+                                        text_nom.setText(jso.getString("temployeur_nom"));
+                                    }else{
+                                        text_nom.setText("");
+                                    }
+                                }catch (JSONException e) {
+                                    Log.e("erreur", e.getMessage());
                                 }
-                            }catch (JSONException e){
-                                Log.e("erreur", e.getMessage());
+                            }
+                        }
+                    });
+                }else{
+                    ServiceHTTPGet serviceHTTPForMat = new ServiceHTTPGet();
+                    serviceHTTPForMat.execute("http://192.168.6.247:3300/retr/pen/selectlike/" + s);
+                    serviceHTTPForMat.setListener(new OnTaskCompleted() {
+                        @Override
+                        public void onTaskCompleted(String response) {
+                            if(response != null) {
+                                try {
+                                    JSONObject jso = new JSONObject(response);
+                                    if (!jso.getString("benficiaire_nom").isEmpty()) {
+                                        text_nom.setText(jso.getString("benficiaire_nom"));
+                                    }else{
+                                        text_nom.setText("");
+                                    }
+                                }catch (JSONException e) {
+                                    Log.e("erreur", e.getMessage());
+                                }
                             }
                         }
                     });
@@ -181,6 +243,7 @@ public class Main3Activity extends AppCompatActivity implements AdapterView.OnIt
 
             @Override
             public void afterTextChanged(Editable s) {
+                Log.e("tag", ""+ s);
             }
         });
 
